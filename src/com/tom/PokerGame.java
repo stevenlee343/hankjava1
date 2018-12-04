4941c6365de8a22dfbb97c79ce6770ececeaccc0
user <stxx0304@gmail.com>
2018-12-04 10:47:04 +0800

    Java 12/4 a full deck

--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -5,16 +5,20 @@ import java.util.Random;
 public class PokerGame {
 
 	public static void main(String[] args) {
-		Random random = new Random();
-		int rcard = random.nextInt(13)+1;
-		String alpha = "SHCD";
-		int A = alpha.length();
-		for(int i = 1; i<=13; i++){
-			System.out.print(rcard);
-			System.out.print(alpha.charAt(random.nextInt(A)));
-			
-			break;
-		}
+		Poker poker = new Poker();
+		poker.qwe();
+		
+		
+//		Random random = new Random();
+//		int rcard = random.nextInt(13)+1;
+//		String alpha = "SHCD";
+//		int A = alpha.length();
+//		for(int i = 1; i<=13; i++){
+//			System.out.print(rcard);
+//			System.out.print(alpha.charAt(random.nextInt(A)));
+//			
+//			break;
+//		}
 
 	}
 	
