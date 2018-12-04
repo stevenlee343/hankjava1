ac738a14ed256e0f82eda9c7d9e6eaa7dc3eb1c4
user <stxx0304@gmail.com>
2018-12-04 11:08:20 +0800

    Java 12/4 advanced poker deck

--- src/com/tom/Poker.java
+++ src/com/tom/Poker.java
@@ -3,44 +3,18 @@ package com.tom;
 import java.util.Random;
 
 public class Poker {
-	int[] cards = new int[52];
+	Card[] cards = new Card[52];
+	
+
 	public Poker(){
 		for(int i=0; i<52; i++){
-			cards[i] = i;
+			cards[i] = new Card(i);
 		}
 	}
 	
 	public void qwe(){
-		for(int i=0; i<52; i++){
-			int flower = cards[i]/13;
-			char c = 0;
-			switch(flower) {
-			case 0:
-				c = 'S';
-				break;
-			case 1:
-				c = 'H';
-				break;
-			case 2:
-				c = 'D';
-				break;
-			case 3:
-				c = 'C';
-				break;
-				
-//			Random random = new Random();
-//			int rcard = random.nextInt(13)+1;
-//			String alpha = "SHCD";
-//			int A = alpha.length();
-			
-//			for(int i = 1; i<=13; i++){
-//				System.out.print(rcard);
-//				System.out.print(alpha.charAt(random.nextInt(A)));
-//				
-//				break;
-			}
-			
-			System.out.print(cards[i]%13+1 + "" + c + " ");
+		for(int i=0; i<52; i++){	
+			System.out.print(cards[i].get() + " ");
 			if(i%13 == 12)
 				System.out.println();
 		}
--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -5,6 +5,12 @@ import java.util.Random;
 public class PokerGame {
 
 	public static void main(String[] args) {
+		int n = 4;
+		int[] inte = new int[52];
+		
+		Card c = new Card(0);
+		System.out.println(c.get());
+		
 		Poker poker = new Poker();
 		poker.qwe();
 		
