604a845db77475e61acd5398257536672ca0dad7
user <user@es302n31>
2018-12-18 11:35:35 +0800

    Java 12/18 change string

--- src/com/tom/Stringtester.java
+++ src/com/tom/Stringtester.java
@@ -7,16 +7,21 @@ public class Stringtester {
 		System.out.println(n != 99);
 		String s1 = "Hello world";
 		//			01234567890
-		String s2 = new String("Hello world");
+		String s2 = new String("Jack Tom Eric");
+		//						0123456789012 
 		System.out.println(s1 == s2);
 		System.out.println(s1.substring(8));
 		System.out.println(s1.substring(2,5));
 		
-		String r = "";
 		for (int i = s1.length() - 1; i >= 0; i--) {
-			r = r + s1.charAt(i);
+			System.out.print(s1.charAt(i));
 			}
-		System.out.println(r);
+		System.out.println();
+		String name = "Tom";
+		int index = s2.indexOf(name);
+		System.out.println(index);
+		System.out.println(s2.substring(0, index) + "boss"
+		+ s2.substring(index + name.length()));
 
 	}
 
